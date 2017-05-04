c3a8e61297376b0e7538f737b4d08e97e01ce692
Matt Hazinski <matt@matthazinski.com>
2017-05-03 20:52:55 -0400

    Change config syntax

--- impala/__init__.py
+++ impala/__init__.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from impala.api import v1
-import impala.config
+from impala import defaults
+import os
 
 app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
+app.config.from_object(defaults)
+
+config_path = os.environ.get('APP_CONFIG_PATH', 'config.py')
+if config_path.endswith('.py'):
+    app.config.from_pyfile(config_path, silent=True)
+else:
+    app.config.from_json(config_path, silent=True)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
---
+++ impala/defaults.py
@@ -0,0 +1,3 @@
+DEBUG = False
+SECRET_KEY = "changeme"
+SQLALCHEMY_DATABASE_URI = "postgresql://impala:changeme@localhost/impala"
