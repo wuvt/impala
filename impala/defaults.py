62d2a3ebfb2fb5cd9784be9b5020043e15726702
Matt Hazinski <matt@matthazinski.com>
2017-05-14 20:26:46 -0400

    Add auth example to default config

--- impala/defaults.py
+++ impala/defaults.py
@@ -1,3 +1,5 @@
 DEBUG = False
 SECRET_KEY = "changeme"
 SQLALCHEMY_DATABASE_URI = "postgresql://impala:changeme@localhost/impala"
+# smuggler:hunter2
+M2M_USERS = {'smuggler': { 'access': ['librarian'], 'password_hash': '$pbkdf2-sha256$29000$g3Du3Zuz1hoDYKx1Tsm5tw$3hZ/b6WaNWaP4Q4zgIpL8nKjaTumekv5l97TkJx4ZBo'}}
