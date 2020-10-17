aa105824c568a541768d5f21f1cae14a186fb39d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-10-16 23:36:52 -0700

    Use SQLAlchemy's upstream pool_pre_ping

--- impala/defaults.py
+++ impala/defaults.py
@@ -1,6 +1,7 @@
 DEBUG = False
 SECRET_KEY = "changeme"
 SQLALCHEMY_DATABASE_URI = "postgresql://impala:changeme@localhost/impala"
+SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
 # smuggler:hunter2
 M2M_USERS = {'smuggler': { 'access': ['librarian'], 'password_hash': '$pbkdf2-sha256$29000$g3Du3Zuz1hoDYKx1Tsm5tw$3hZ/b6WaNWaP4Q4zgIpL8nKjaTumekv5l97TkJx4ZBo'}}
 ENABLE_OIDC = False
